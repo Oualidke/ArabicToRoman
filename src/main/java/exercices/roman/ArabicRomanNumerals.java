46de1e829fc67a617626c1e7fe0f05921067ce0f
Oualidke <oualidkerrou@gmail.com>
2024-12-18 21:14:21 +0100

    Conversion 1 en I (corrigé)

--- src/main/java/exercices/roman/ArabicRomanNumerals.java
+++ src/main/java/exercices/roman/ArabicRomanNumerals.java
@@ -3,7 +3,9 @@ package exercices.roman;
 public class ArabicRomanNumerals {
 
     public static String convert(int nbr) {
-
+    if (nbr == 1){
+        return "I";
+    }
         return null;
     }
 }
