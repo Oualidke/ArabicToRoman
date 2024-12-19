99f42f8b7d8ab7eff614db637b458559a6f1e0aa
Oualidke <oualidkerrou@gmail.com>
2024-12-19 09:42:04 +0100

    Conversion de 3 en III (Corrigé)

--- src/main/java/exercices/roman/ArabicRomanNumerals.java
+++ src/main/java/exercices/roman/ArabicRomanNumerals.java
@@ -4,7 +4,7 @@ public class ArabicRomanNumerals {
     public static String convert(int nbr) {
         if (nbr == 1) return "I";
         if (nbr == 2) return "II";
-
+        if (nbr == 3) return "III";
         return "";
     }
 }
